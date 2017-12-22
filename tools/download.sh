f89e773e25bcee17708efec0e72fb00877711e07
Hengyuan Hu <kevin.huhy@gmail.com>
2017-12-22 00:19:25 -0500

    moved scripts to tools

--- tools/download.sh
+++ tools/download.sh
@@ -16,18 +16,18 @@ rm data/v2_Questions_Val_mscoco.zip
 
 wget -P data http://visualqa.org/data/mscoco/vqa/v2_Questions_Test_mscoco.zip
 unzip data/v2_Questions_Test_mscoco.zip -d data
-rm v2_Questions_Test_mscoco.zip
+rm data/v2_Questions_Test_mscoco.zip
 
 # Annotations
 wget -P data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Train_mscoco.zip
 unzip data/v2_Annotations_Train_mscoco.zip -d data
-rm v2_Annotations_Train_mscoco.zip
+rm data/v2_Annotations_Train_mscoco.zip
 
 wget -P data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 unzip data/v2_Annotations_Val_mscoco.zip -d data
-rm v2_Annotations_Val_mscoco.zip
+rm data/v2_Annotations_Val_mscoco.zip
 
 # Image Features
 wget -P data https://storage.googleapis.com/bottom-up-attention/trainval_36.zip
 unzip data/trainval_36.zip -d data
-rm trainval_36.zip
+rm data/trainval_36.zip
